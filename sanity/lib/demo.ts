bd1838531a02d8776d25a30078d10d729614ef8c
UsmanFaisal13 <130107471+UsmanFaisal13@users.noreply.github.com>
2025-02-03 23:31:47 +0500

    first commit

---
+++ sanity/lib/demo.ts
@@ -0,0 +1,30 @@
+/**
+ * Demo data used as placeholders and initial values for the blog
+ */
+
+export const title = "STEM NEWS";
+
+export const description = [
+  {
+    _key: "9f1a629887fd",
+    _type: "block",
+    children: [
+      {
+        _key: "4a58edd077880",
+        _type: "span",
+        marks: [],
+        text: "News and updates on the world of Science and Technology ",
+      },
+      {
+        _key: "4a58edd077884",
+        _type: "span",
+        marks: [],
+        text: ".",
+      },
+    ],
+
+    style: "normal",
+  },
+];
+
+export const ogImageTitle = "A Next.js Blog with a Native Authoring Experience";
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,17 @@
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
 
-const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+export default {
+  content: ["./app/**/*.{ts,tsx}", "./sanity/**/*.{ts,tsx}"],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        sans: ["var(--font-inter)"],
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  plugins: [typography],
+} satisfies Config;
